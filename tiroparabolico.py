6af2a68e62221f71940e2e39ea1a5bfd948c9143
Felipe <A01662377@tec.mx>
2025-10-31 03:24:01 -0600

    juego inifinito

--- tiroparabolico.py
+++ tiroparabolico.py
@@ -57,7 +57,8 @@ def move():
 
     for target in targets:
         if not inside(target):
-            return
+            target.x = 200
+            target.y = randrange(-150, 150)
 
     ontimer(move, 23)
 
